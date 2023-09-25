64b81a2b210b6b488080e413b0127fc4667509b7
AnshukGottipati <anshukgp18@gmail.com>
2023-09-25 16:45:15 -0400

    FinalUpdate

--- TableTemplate.js
+++ TableTemplate.js
@@ -2,56 +2,61 @@ class TableTemplate{
 
     static fillIn(id,dict,columnName = " "){
         var table = document.getElementById(id);
+        //number of rows
         var rowCount = table.rows.length;
-        var colCount = table.rows[0].cells.length;
         console.log(rowCount);
+        //number of columns
+        var colCount = table.rows[0].cells.length;
         console.log(colCount);
-        var storage = "";
-        var indices = table.rows.item(0).cells;
+        var innerFill = null;
+        
+        //first cell[0,0]
+        var col1 = table.rows[0].cells[0];
+        col1.innerHTML = extractString(String(col1.innerHTML),dict);
+        //second cell[0,1]
+        var col2 = table.rows[0].cells[1];
+        col2.innerHTML = extractString(String(col2.innerHTML),dict);
 
-        var indexFill = null;
-        //replaces strings in first row
-        for(var j = 0; j < colCount;j++){
-            
-            storage = String(indices.item(j).innerHTML);
-            indices[j].innerHTML = this.extractString(storage,dict);
-            if(indices[j].innerHTML == columnName){
-                indexFill = j;
+        if(columnName == " "){
+            for(var i = 1; i < rowCount;i++){
+                for(var j = 0; j < colCount;j++){
+                    var temp = table.rows[i].cells[j];
+                    temp.innerHTML = extractString(String(temp.innerHTML),dict);
+                }
+            }
+        
+        }else if(col1.innerHTML == columnName){
+            for(var i = 0; i < rowCount;i++){
+                var temp = table.rows[i].cells[0];
+                temp.innerHTML = extractString(String(temp.innerHTML),dict);
+            }
+        }else if(col2.innerHTML == columnName){
+            for(var i = 0; i < rowCount;i++){
+                var temp = table.rows[i].cells[1];
+                temp.innerHTML = extractString(String(temp.innerHTML),dict);
             }
         }
         
-        columnName = " ";
-        //change whole table since not specified
-        if(columnName == " "){
-            for(var i = 1; i < rowCount;i++){
-                var indices = table.rows.item(i).cells;
-                for(var j = 0; j < colCount;j++){
-                    storage = String(indices.item(j).innerHTML);
-                    indices[j].innerHTML = this.extractString(storage,dict);
+        function extractString(template,dictionary){
+            var str = template;
+            const matches = str.match(/{{.*?}}/g);
+    
+            for (const i in matches){
+                if (matches[i] != null){
+                    const match = matches[i];
+                    const value = dictionary[match.substring(2, match.indexOf("}}"))] || " ";
+                    str = str.replace(match, value);
                 }
             }
-        //if is specified
-        } else if( indexFill != null){
-            for(var i = 1; i < rowCount;i++){
-                var indices = table.rows.item(i).cells;
-                storage = String(indices.item(indexFill).innerHTML);
-                indices[indexFill].innerHTML = this.extractString(storage,dict);
-            }
-        } 
-
+            
+            
+            return str;
+    
+                
+        }
 
     } 
-    extractString(text, dictionary){
-        let returnString = text;
-        for (const property in dictionary){
-            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
-                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
-            }
-        }
-        const regex = /{{.*}}/g;
-        returnString = returnString.replaceAll(regex,"");
-        return returnString;
-    }
+    
     
 
 }
